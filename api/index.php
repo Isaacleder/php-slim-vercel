e7a147edd661a9627d13fdb71a6dcb10efc56669
isaac.leder <isaac.leder@alunos.se.senac.br>
2022-12-15 14:58:44 -0300

    ok

---
+++ api/index.php
@@ -0,0 +1,36 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+$app->get('/hello/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("Hello World!");
+    return $response;
+});
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("Index!");
+    return $response;
+});
+
+$app->get('/testbot/', function (Request $request, Response $response, array $args) {
+    
+    require_once ("controllers/ControllerApiTelegram.php");
+    
+    ControllerApiTelegram::sendMessage("teste chatbot de isaac");
+    
+    $response->getBody()->write("Enviando mensagem para o chatbot!");
+});
+
+$app->run();
